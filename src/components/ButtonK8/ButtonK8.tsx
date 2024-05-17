b14e98c7cbc43c321d600d4098112ef0c96bfc0f
Catto <catto@live.com>
2024-05-17 10:40:50 -0400

    updated package.json to add publishConfig for npm package.

--- src/components/ButtonK8/ButtonK8.tsx
+++ src/components/ButtonK8/ButtonK8.tsx
@@ -4,8 +4,8 @@ export interface ButtonProps {
   label: string;
 }
 
-const Button = (props: ButtonProps) => {
+const ButtonK8 = (props: ButtonProps) => {
   return <button>{props.label}</button>;
 };
 
-export default Button;
+export default ButtonK8;
